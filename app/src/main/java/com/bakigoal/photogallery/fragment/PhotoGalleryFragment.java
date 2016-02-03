669486660228c2114dedaa36006225caef151998
i.bakirov <ilmirbakirov@gmail.com>
2016-02-03 23:24:19 +0500

    get from flikr service data

--- app/src/main/java/com/bakigoal/photogallery/fragment/PhotoGalleryFragment.java
+++ app/src/main/java/com/bakigoal/photogallery/fragment/PhotoGalleryFragment.java
@@ -1,67 +1,74 @@
 package com.bakigoal.photogallery.fragment;
 
 
-
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
 
 import com.bakigoal.photogallery.R;
+import com.bakigoal.photogallery.model.GalleryItem;
+import com.bakigoal.photogallery.util.FlickrFetchr;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link PhotoGalleryFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class PhotoGalleryFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+  private static final String TAG = "PhotoGalleryFragment";
+  List<GalleryItem> items;
+  private GridView gridView;
 
+  public PhotoGalleryFragment() {
+    // Required empty public constructor
+  }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PhotoGalleryFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PhotoGalleryFragment newInstance(String param1, String param2) {
-        PhotoGalleryFragment fragment = new PhotoGalleryFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setRetainInstance(true);
+    new FetchItemsTask().execute();
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                           Bundle savedInstanceState) {
+    // Inflate the layout for this fragment
+    View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+    gridView = (GridView) view.findViewById(R.id.gridView);
+    setupAdapter();
+    return view;
+  }
+
+  private void setupAdapter() {
+    if (getActivity() == null || gridView == null) {
+      return;
     }
-    public PhotoGalleryFragment() {
-        // Required empty public constructor
+    if (items != null) {
+      gridView.setAdapter(new ArrayAdapter<>(getActivity(),
+          android.R.layout.simple_gallery_item, items));
+    } else {
+      gridView.setAdapter(null);
+    }
+  }
+
+  private class FetchItemsTask extends AsyncTask<Void, Void, List<GalleryItem>> {
+
+    @Override
+    protected List<GalleryItem> doInBackground(Void... params) {
+      items = new FlickrFetchr().fetchItems();
+      return items;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    protected void onPostExecute(List<GalleryItem> galleryItems) {
+      items = galleryItems;
+      setupAdapter();
     }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_photo_gallery, container, false);
-    }
+  }
 
 }
---
+++ app/src/main/java/com/bakigoal/photogallery/model/GalleryItem.java
@@ -0,0 +1,38 @@
+package com.bakigoal.photogallery.model;
+
+/**
+ * Created by ilmir on 03.02.16.
+ */
+public class GalleryItem {
+  private String caption;
+  private String id;
+  private String url;
+
+  public String getCaption() {
+    return caption;
+  }
+
+  public void setCaption(String caption) {
+    this.caption = caption;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public String getUrl() {
+    return url;
+  }
+
+  public void setUrl(String url) {
+    this.url = url;
+  }
+
+  public String toString() {
+    return caption;
+  }
+}
